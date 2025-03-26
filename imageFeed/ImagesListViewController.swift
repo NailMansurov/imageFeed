6f47c959fb17d2a0170c8d43c373872439853856
Nail Mansurov <mansurov-nf@live.com>
2025-03-27 00:09:01 +0500

    Сделано задание (Спринт 8/26 Тема 4/5 "Практика по работе с таблицей в проекте" Урок 2/5)

---
+++ imageFeed/ImagesListViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class ImagesListViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
+
