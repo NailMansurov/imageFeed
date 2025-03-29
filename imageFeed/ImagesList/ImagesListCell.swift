8d5c8e693061adfef1912e683ba913d62ed82eef
Nail Mansurov <mansurov-nf@live.com>
2025-03-29 22:12:57 +0500

    Правки по рекомендациям после ревью.

--- imageFeed/ImagesList/ImagesListCell.swift
+++ imageFeed/ImagesList/ImagesListCell.swift
@@ -6,7 +6,18 @@ final class ImagesListCell: UITableViewCell {
     
     static let reuseIdentifier = "ImagesListCell"
     
-    @IBOutlet var likeButton: UIButton!
-    @IBOutlet var dateLabel: UILabel!
-    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet private var likeButton: UIButton!
+    @IBOutlet private var dateLabel: UILabel!
+    @IBOutlet private var cellImage: UIImageView!
+    
+    func configure(with image: UIImage, date: String, isLiked: Bool) {
+        cellImage.image = image
+        dateLabel.text = date
+        
+        let likeImage = isLiked ? UIImage(named: "likeButtonOn") : UIImage(
+            named: "likeButtonOff"
+        )
+        
+        likeButton.setImage(likeImage, for: .normal)
+    }
 }
--- imageFeed/ImagesList/ImagesListViewController.swift
+++ imageFeed/ImagesList/ImagesListViewController.swift
@@ -71,6 +71,11 @@ extension ImagesListViewController: UITableViewDelegate {
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
         let imageWidth = image.size.width
+        
+        guard imageWidth > 0 else {
+            return 0
+        }
+        
         let scale = imageViewWidth / imageWidth
         let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
         
@@ -84,14 +89,9 @@ extension ImagesListViewController {
             return
         }
         
-        cell.cellImage.image = image
-        cell.dateLabel.text = dateFormatter.string(from: Date())
-        
+        let date = dateFormatter.string(from: Date())
         let isLiked = indexPath.row % 2 == 0
-        let likeImage = isLiked ? UIImage(named: "likeButtonOn") : UIImage(
-            named: "likeButtonOff"
-        )
         
-        cell.likeButton.setImage(likeImage, for: .normal)
+        cell.configure(with: image, date: date, isLiked: isLiked)
     }
 }
