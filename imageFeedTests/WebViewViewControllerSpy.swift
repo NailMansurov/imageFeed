5b19e6e46252be75c865c4310ecd06118ea3fa27
Nail Mansurov <mansurov-nf@live.com>
2025-08-04 23:26:45 +0500

    test: Добавлен тест WebViewViewController

---
+++ imageFeedTests/WebViewViewControllerSpy.swift
@@ -0,0 +1,20 @@
+import imageFeed
+import Foundation
+
+final class WebViewViewControllerSpy: WebViewViewControllerProtocol {
+    var presenter: imageFeed.WebViewPresenterProtocol?
+    
+    var loadRequestCalled: Bool = false
+    
+    func load(request: URLRequest) {
+        loadRequestCalled = true
+    }
+    
+    func setProgressValue(_ newValue: Float) {
+        
+    }
+    
+    func setProgressHidden(_ isHidden: Bool) {
+        
+    }
+}
